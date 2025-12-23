cd945e0d03c60da95274072a23fa67b34d029c5a
Donnie <mater013@umn.edu>
2025-12-22 18:02:24 -0600

    Introduction of IOCOmponent

--- include/registration/IoTropolisUnitConnection.h
+++ include/registration/IoTropolisUnitConnection.h
@@ -4,26 +4,45 @@
 #include <QObject>
 #include <QTcpSocket>
 #include <QStringList>
+#include <QList>
+
+#include "registration/IOComponent.h"
 
 constexpr int MAX_UNKNOWN_COMMANDS = 5;
 
-// Type alias for unit ID (can be extended later)
+// Type alias for unit ID
 using UnitID = quint32;
 
 class IoTropolisUnitConnection : public QObject
 {
     Q_OBJECT
 public:
-    explicit IoTropolisUnitConnection(QTcpSocket* socket, QObject* parent = nullptr);
+    explicit IoTropolisUnitConnection(QTcpSocket* socket,
+                                      QObject* parent = nullptr);
 
-    QString ipAddress() const;  // readable IP
-    QString unitType() const { return m_unitType; }
+    // --------------------------------------------------------
+    // Connection info
+    // --------------------------------------------------------
+    QString ipAddress() const;
+
+    // --------------------------------------------------------
+    // IO Components
+    // --------------------------------------------------------
+    QList<IOComponent> sensors() const   { return m_sensors; }
+    QList<IOComponent> actuators() const { return m_actuators; }
+
+    // Convenience accessors
+    QStringList sensorNames() const;
+    QStringList actuatorNames() const;
+
+    // --------------------------------------------------------
+    // Unit metadata
+    // --------------------------------------------------------
+    QString unitType() const    { return m_unitType; }
     QString unitSubtype() const { return m_unitSubtype; }
-    QStringList sensors() const { return m_sensors; }
-    QStringList actuators() const { return m_actuators; }
 
-    UnitID unitID() const { return m_unitID; }
-    void setUnitID(UnitID id) { m_unitID = id; }
+    UnitID unitID() const       { return m_unitID; }
+    void setUnitID(UnitID id)   { m_unitID = id; }
 
 signals:
     void helloCompleted();
@@ -36,21 +55,29 @@ private slots:
     void onDisconnected();
 
 private:
-    void failProtocol(const QString& msg, const QString& clientMsg = QString());
+    // --------------------------------------------------------
+    // Protocol helpers
+    // --------------------------------------------------------
+    void failProtocol(const QString& msg,
+                      const QString& clientMsg = QString());
     void sendReply(const QString& msg);
     void resetUnknownCommandCounter();
 
-    QTcpSocket* m_socket;
+    // --------------------------------------------------------
+    // Data members
+    // --------------------------------------------------------
+    QTcpSocket* m_socket{nullptr};
 
-    bool m_helloDone;
-    bool m_describeDone;
+    bool m_helloDone{false};
+    bool m_describeDone{false};
 
     QString m_unitType;
     QString m_unitSubtype;
-    QStringList m_sensors;
-    QStringList m_actuators;
 
-    int m_unknownCommandCount;
+    QList<IOComponent> m_sensors;
+    QList<IOComponent> m_actuators;
+
+    int m_unknownCommandCount{0};
 
     UnitID m_unitID{0}; // Assigned by server
 };
